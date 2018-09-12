7fe1986b666f392e0c05a77831b36f896e05c936
Fey Ijaware <Fey.Ijaware@peninsula-uk.com>
2018-09-12 09:13:53 +0100

    fourth commit: Expanded the Tour of Heroes app to display a list of heroes, and allow users to select a hero and display the hero's details.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,17 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
+  { id: 0, name: 'Dr IQ' },
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Dr IQ' },
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
