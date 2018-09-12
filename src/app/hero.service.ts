d5d378c1750d5a2f311b63a6383f2681c3ae8060
Fey Ijaware <Fey.Ijaware@peninsula-uk.com>
2018-09-12 11:57:52 +0100

    updated angular code and cli and created a HeroService that all application classes can use to get heroes and a MessageService that will display a message when HeroService fetches heroes successfully

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
-@Injectable()
+ 
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
+  getHeroes(): Observable<Hero[]> {
+  // TODO: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: Fetched heroes');
+  return of (HEROES);
+}
 }
